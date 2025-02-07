84433e1b833a55f14dc345d570ec6d02e93aab0f
AndersonBarbosa-cpu <anderson_barbosadasilva@hotmail.com>
2025-02-07 17:02:27 -0300

    criei a pagina principal

---
+++ site-exemplo/script.js
@@ -0,0 +1,8 @@
+// Exemplo de função para algo dinâmico, como enviar um alerta ou interações
+function enviarFormulario(event) {
+    event.preventDefault();
+    alert('Mensagem enviada com sucesso!');
+  }
+  
+  
+  
